66b038c96dddb4827b8cc97d4fffce686677bce6
0.0repeating <weranoutofgoodnames@gmail.com>
2016-01-30 02:53:04 -0600

    SSG's in, and the DECORATE's as horrifying as ever
    i'm sorry

--- pk3/acs/dakka_tempitems.h
+++ pk3/acs/dakka_tempitems.h
@@ -1,13 +1,11 @@
-#define TMPITEM_COUNT       8
+#define TMPITEM_COUNT       6
 
 #define TMP_LEFTFIRE        0
 #define TMP_RIGHTFIRE       1
 #define TMP_LEFTCLICK       2
 #define TMP_RIGHTCLICK      3
-#define TMP_SSGAGAIN        4
-#define TMP_RELOADSSG       5
-#define TMP_INFINITEAMMO    6
-#define TMP_CLASSITEM       7
+#define TMP_INFINITEAMMO    4
+#define TMP_CLASSITEM       5
 
 int TempChecks[TMPITEM_COUNT];
 
@@ -17,8 +15,6 @@ int TempItems[TMPITEM_COUNT] =
     "AltFired",
     "MainClicked",
     "AltClicked",
-    "FireSSGAgain",
-    "ReloadSSG",
     "DakkaInfiniteAmmo",
     "HUD_IsDakkaguy",
 };
@@ -31,9 +27,6 @@ function void Dakka_UpdateTemporaryItems(void)
     TempChecks[TMP_RIGHTFIRE]       = keyDown(BT_ALTATTACK);
     TempChecks[TMP_LEFTCLICK]       = keyPressed(BT_ATTACK);
     TempChecks[TMP_RIGHTCLICK]      = keyPressed(BT_ALTATTACK);
-
-    TempChecks[TMP_SSGAGAIN]        = false;
-    TempChecks[TMP_RELOADSSG]       = false;
 
     int classNum = Pickup_ClassNumber(0);
 
