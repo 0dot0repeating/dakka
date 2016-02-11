a80d8daff874b803f9f52f8cddc5bc75d0c789c0
0.0repeating <weranoutofgoodnames@gmail.com>
2016-02-10 19:09:09 -0600

    Super shotgun ACS calls moved to named scripts - this is now incompatible with Zandronum 2

--- pk3/acs/dakka_decorate.h
+++ pk3/acs/dakka_decorate.h
@@ -5,10 +5,6 @@ int RotatingSoundIndex[PLAYERMAX];
 #define DEC_SCRAP_XYVEL     1
 #define DEC_SCRAP_ZVEL      2
 
-#define DEC_SSG_SHOTSLEFT   3
-#define DEC_SSG_FIRED       4
-#define DEC_SSG_BOTHCLICKED 5
-#define DEC_SSG_REFIRE      6
 #define DEC_RL_ALTFIRE      7
 
 script DAKKA_DECORATE (int mode, int a1, int a2)
@@ -47,37 +43,6 @@ script DAKKA_DECORATE (int mode, int a1, int a2)
             ret = FixedDiv(ret, mag);
         }
 
-        break;
-
-      case DEC_SSG_SHOTSLEFT:
-        // SSG_SHOTMAX is defined in dakka_const.h for right now
-        ret = SSG_SHOTMAX - CheckInventory("DakkaSSG_ShotsFired");
-        break;
-
-
-    // Only reason this exists is because A_GiveInventory occasionally desyncs
-    //  online. This doesn't desync. As often. I think.
-      case DEC_SSG_FIRED:
-        if (a1 == 0) { a1 = 1; }
-
-        if (a1 < 0)
-        {
-            TakeInventory("DakkaSSG_ShotsFired", -a1);
-            SSG_AutoReloadCooldown[pln] = 0;
-        }
-        else
-        {
-            GiveInventory("DakkaSSG_ShotsFired",  a1);
-            SSG_AutoReloadCooldown[pln] = 35;
-        }
-        break;
-
-      case DEC_SSG_BOTHCLICKED:
-        ret = keyPressed(BT_ATTACK | BT_ALTATTACK);
-        break;
-
-      case DEC_SSG_REFIRE:
-        ret = keyPressed_any(BT_ATTACK | BT_ALTATTACK) && (CheckInventory("DakkaSSG_ShotsFired") < SSG_SHOTMAX);
         break;
 
       case DEC_RL_ALTFIRE:
