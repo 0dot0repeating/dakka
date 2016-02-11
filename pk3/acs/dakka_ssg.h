f0bc6ccc201fd78aaa48f5761af885963b88215c
Ijon <segfaultsamurai@gmail.com>
2016-02-10 19:09:09 -0600

    Super shotgun ACS calls moved to named scripts - this is now incompatible with Zandronum 2

--- pk3/acs/dakka_ssg.h
+++ pk3/acs/dakka_ssg.h
@@ -23,3 +23,43 @@ function void SSG_AutoReload(void)
 
     SSG_AutoReloadCooldown[pln] = time;
 }
+
+
+
+script "DSSG_ShotsLeft" (void)
+{
+    SetResultValue(SSG_SHOTMAX - CheckInventory("DakkaSSG_ShotsFired"));
+}
+
+// Only reason this exists is because A_GiveInventory occasionally desyncs
+//  online. This doesn't desync. As often. I think.
+script "DSSG_ChangeShots" (int amount)
+{
+    int pln = PlayerNumber();
+    
+    if (amount == 0) { amount = 1; }
+
+    if (amount < 0)
+    {
+        TakeInventory("DakkaSSG_ShotsFired", -amount);
+        SSG_AutoReloadCooldown[pln] = 0;
+    }
+    else
+    {
+        GiveInventory("DakkaSSG_ShotsFired",  amount);
+        SSG_AutoReloadCooldown[pln] = 35;
+    }
+}
+
+script "DSSG_BothClicked" (void)
+{
+    SetResultValue(keyPressed(BT_ATTACK | BT_ALTATTACK));
+}
+
+script "DSSG_Refire" (void)
+{
+    int ret = keyPressed_any(BT_ATTACK | BT_ALTATTACK)
+              && CheckInventory("DakkaSSG_ShotsFired");
+
+    SetResultValue(ret);
+}
