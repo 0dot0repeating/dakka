3bf6d7c0e5e3683a1d4b0c7d6773c2698fb1f59c
0.0repeating <weranoutofgoodnames@gmail.com>
2016-03-06 16:19:09 -0600

    Moved the sound rotating script from DAKKA_DECORATE to "Dakka_RotateSound"

--- pk3/acs/dakka_decorate.h
+++ pk3/acs/dakka_decorate.h
@@ -1,6 +1,3 @@
-// This is used to run weapon sounds on different slots automagically.
-int RotatingSoundIndex[PLAYERMAX];
-
 #define DEC_ROTATESOUND     0
 #define DEC_SCRAP_XYVEL     1
 #define DEC_SCRAP_ZVEL      2
@@ -13,14 +10,6 @@ script DAKKA_DECORATE (int mode, int a1, int a2)
 
     switch (mode)
     {
-      case DEC_ROTATESOUND:
-        int leftBound  = cond(a1 == 0, 4, a1);
-        int rightBound = cond(a2 == 0, 7, a2);
-        int indexRange = (rightBound - leftBound) + 1;
-
-        ret = (RotatingSoundIndex[pln]++ % indexRange) + leftBound;
-        break;
-
       case DEC_SCRAP_XYVEL:
       case DEC_SCRAP_ZVEL:
         int x = GetActorVelX(0);
