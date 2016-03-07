01f59fa851f74f8c15ea1e43173f783184782cf6
0.0repeating <weranoutofgoodnames@gmail.com>
2016-03-07 02:08:56 -0600

    Tweaked revive effect, added debug score functions

--- pk3/acs/score/score.h
+++ pk3/acs/score/score.h
@@ -6,3 +6,22 @@
 #include "score_weapons.h"
 #include "score_reward.h"
 #include "score_levelstart.h"
+
+
+
+// Debug functions
+
+script "Score_GiveRewards" (int rewardCount)
+{
+    Score_ModScore(PlayerNumber(), MapStart_FullHealPoints * max(rewardCount, 0));
+}
+
+script "Score_GiveLives" (int lifeCount)
+{
+    Score_ModExtraLives(PlayerNumber(), lifeCount);
+}
+
+script "Score_GiveRegen" (int regenCount)
+{
+    Score_ModRegenTimer(PlayerNumber(), regenCount);
+}
