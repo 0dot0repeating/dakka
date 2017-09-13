843e04db43233df5716d9f7f3270c33b0f9b4b95
0.0repeating <weranoutofgoodnames@gmail.com>
2017-09-13 04:05:14 -0500

    Added impaler alt-fire. Spoiler alert: it impales things

--- pk3/acs/dakka_rocketlauncher.h
+++
@@ -1,6 +0,0 @@
-script "Dakka_RLFull" (void)
-{
-    int ret = (CheckInventory("DakkaRockets") - CheckInventory("DakkaRocketsLoaded")) > 0;
-    ret |= CheckInventory("DakkaInfiniteAmmo");
-    SetResultValue(ret);
-}
