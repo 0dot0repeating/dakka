477ba4aedaab0b9745ce1b1286e866cbff50bac6
0.0repeating <weranoutofgoodnames@gmail.com>
2017-12-16 20:54:36 -0600

    [DAKKTEST] The target now reports how much damage it took

---
+++ pk3/acs/dakka_testmap.c
@@ -0,0 +1,14 @@
+#library "dakka_testmap"
+#include "zcommon.acs"
+
+script "DAKKTEST_ReportDamage" (void)
+{
+    int hp    = GetActorProperty(0, APROP_Health);
+    int maxhp = GetActorProperty(0, APROP_SpawnHealth);
+    
+    if (hp < maxhp)
+    {
+        Log(s:"\cd[", d:Timer(), s:"]\c- Dealt ", d:maxhp - hp, s:" damage to target"); 
+    }
+    SetActorProperty(0, APROP_Health, maxhp);
+}
