fa4846e744eb5bd7d2a83a166cf3c7dd211ea72d
ijon <segfaultsamurai@gmail.com>
2017-12-19 21:29:50 -0600

    You can now deliver a RIGHTEOUS FINISHER with your punching (use alt-fire)

--- pk3/acs/dakka_fists.h
+++ pk3/acs/dakka_fists.h
@@ -7,9 +7,12 @@ function void Dakka_FistTap(void)
     else { TakeInventory("DakkaFistTap_Alt", 1); }
 }
 
-script "Dakka_FistHit" (void)
+script "Dakka_FistHit" (int thrustpower, int lockpower)
 {
     int myTID = defaultTID(0);
+    if (lockpower == 0) { lockpower = 6; }
+    if (lockpower < 0)  { lockpower = 0; }
+    lockpower = itof(lockpower) / 360;
     
     SetActivator(0, AAPTR_TARGET);
     if (ClassifyActor(0) & ACTOR_WORLD)
@@ -30,14 +33,22 @@ script "Dakka_FistHit" (void)
     
     SetResultValue(true);
     
-    int dx = GetActorX(0) - GetActorX(firerTID);
-    int dy = GetActorY(0) - GetActorY(firerTID);
+    if (thrustpower > 0)
+    {
+        ACS_NamedExecuteWithResult("Dakka_FistThrust", firerAngle, thrustpower);
+    }
     
-    int punchAngle = VectorAngle(dx, dy);
-    int angleDiff  = middle(-0.02, angleDifference(firerAngle, punchAngle), 0.02);
-    
-    SetActivator(firerTID);
-    ACS_NamedExecuteWithResult("Dakka_FistLockon", angleDiff);
+    if (lockpower > 0)
+    {
+        int dx = GetActorX(0) - GetActorX(firerTID);
+        int dy = GetActorY(0) - GetActorY(firerTID);
+        
+        int punchAngle = VectorAngle(dx, dy);
+        int angleDiff  = middle(-lockpower, angleDifference(firerAngle, punchAngle), lockpower);
+        
+        SetActivator(firerTID);
+        ACS_NamedExecuteWithResult("Dakka_FistLockon", angleDiff);
+    }
 }
 
 
@@ -53,4 +64,17 @@ script "Dakka_FistLockon" (int angDiff) clientside
     }
     
     ChangeActorAngle(0, GetActorAngle(0) + left, true);
+}
+
+
+script "Dakka_FistThrust" (int angle, int power)
+{
+    int myMass   = GetActorProperty(0, APROP_Mass);
+    int adjPower = FixedSqrt((itof(power) / myMass) * 200);
+    int thrustX  = FixedMul(cos(angle), adjPower);
+    int thrustY  = FixedMul(sin(angle), adjPower);
+    
+    Log(f:thrustX, s:", ", f:thrustY);
+    
+    SetActorVelocity(0, thrustX, thrustY, GetActorVelZ(0), false, false);
 }
