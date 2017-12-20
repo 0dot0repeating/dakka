506babd0664dc285d839c4fc61e5b78c7206fa17
0.0repeating <weranoutofgoodnames@gmail.com>
2017-12-19 18:53:03 -0600

    Simplified down fist code, removed the LeftHit/RightHit states (don't like em)

--- pk3/acs/dakka_fists.h
+++ pk3/acs/dakka_fists.h
@@ -12,69 +12,32 @@ script "Dakka_FistHit" (void)
     int myTID = defaultTID(0);
     
     SetActivator(0, AAPTR_TARGET);
-    int firerTID      = defaultTID(0);
-    int punchSpeed    = CheckInventory("DakkaFistSpeed");
-    int isBerserked   = CheckInventory("PowerStrength");
-    int primaryTapped = CheckInventory("DakkaFistTap_Main") > 0;
-    int finisher      = primaryTapped && (punchSpeed >= 25);
-    
-    SetActivator(myTID, AAPTR_TRACER);
     if (ClassifyActor(0) & ACTOR_WORLD)
     {
-        SetActivator(myTID);
-        ACS_NamedExecuteWithResult("Dakka_FistMiss");
+        SetResultValue(false);
         terminate;
     }
     
-    int hitTID_old   = ActivatorTID();
-    int hitTID_new   = defaultTID(0);
-    int hitShootable = CheckFlag(0, "SHOOTABLE");
+    int firerTID   = defaultTID(0);
+    int firerAngle = GetActorAngle(0);
     
-    if (!hitShootable)
+    SetActivator(myTID, AAPTR_TRACER);
+    if ((ClassifyActor(0) & ACTOR_WORLD) || (!CheckFlag(0, "SHOOTABLE")))
     {
-        Thing_ChangeTID(hitTID_new, hitTID_old);
-        SetActivator(myTID);
-        ACS_NamedExecuteWithResult("Dakka_FistMiss");
+        SetResultValue(false);
         terminate;
     }
     
-    int hitDamage = 7 + (min(15, punchSpeed) / 5);
-    if (isBerserked) { hitDamage *= 3; }
+    SetResultValue(true);
     
-    GiveActorInventory(firerTID, "DakkaFistSpeed",  2);
-    GiveActorInventory(firerTID, "DakkaFistQuake1", 1);
-    ThingSound(myTID, "dakka/punch", 127);
+    int dx = GetActorX(0) - GetActorX(firerTID);
+    int dy = GetActorY(0) - GetActorY(firerTID);
     
-    SetActivator(myTID);
-    ACS_NamedExecuteWithResult("Dakka_FistDamage", hitDamage);
+    int punchAngle = VectorAngle(dx, dy);
+    int angleDiff  = middle(-0.02, angleDifference(firerAngle, punchAngle), 0.02);
     
-    Thing_ChangeTID(hitTID_new, hitTID_old);
-}
-
-script "Dakka_FistDamage" (int damage)
-{
-    int myTID = defaultTID(-1);
-
-    SetActivator(0, AAPTR_TRACER);
-    if (ClassifyActor(0) & ACTOR_WORLD) { terminate; }
-
-    int hisTID     = ActivatorTID();
-    int hisTID_new = defaultTID(-1);
-
-    SetActivator(myTID, AAPTR_TARGET);
-    Thing_Damage2(hisTID_new, damage, "Punch");
-    
-    int oldAng = GetActorAngle(0);
-    int newAng = VectorAngle(GetActorX(hisTID_new) - GetActorX(0),
-                             GetActorY(hisTID_new) - GetActorY(0));
-    
-    int angDiff = angleDifference(oldAng, newAng);
-    
-    if      (angDiff >  0.02) { angDiff =  0.02; }
-    else if (angDiff < -0.02) { angDiff = -0.02; }
-    
-    ACS_NamedExecuteAlways("Dakka_FistLockon", 0, angDiff);
-    Thing_ChangeTID(hisTID_new, hisTID);
+    SetActivator(firerTID);
+    ACS_NamedExecuteWithResult("Dakka_FistLockon", angleDiff);
 }
 
 
@@ -90,21 +53,4 @@ script "Dakka_FistLockon" (int angDiff) clientside
     }
     
     ChangeActorAngle(0, GetActorAngle(0) + left, true);
-}
-
-
-script "Dakka_FistBlock" (int reflectTime, int resistTime)
-{
-    GiveInventory("DakkaBlockResistance", 1);
-    GiveInventory("DakkaBlockReflection", 1);
-    
-    int t = 0;
-    int endTime = max(reflectTime, resistTime);
-    
-    for (t = 0; t <= endTime; t++)
-    {
-        if (t == reflectTime) { GiveInventory("DakkaUnblockReflection", 1);          }
-        if (t == resistTime)  { TakeInventory("DakkaBlockResistance",   0x7FFFFFFF); }
-        Delay(1);
-    }
 }
