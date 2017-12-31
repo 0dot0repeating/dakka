56fefbde9d96817c7642ee27e0538edb71ec4be1
ijon <segfaultsamurai@gmail.com>
2017-12-31 13:37:33 -0600

    Split step 12 into two steps, implemented steps 10 to 14

--- pk3/acs/arc/arc_main.h
+++ pk3/acs/arc/arc_main.h
@@ -1,5 +1,8 @@
 script "Arc_Main" (int arcType)
 {
+    // sanity check
+    if (arcType < 0 || arcType >= ARCTYPES) { terminate; }
+    
     // step 1 already finished
     int arcerTID = defaultTID(0);
     
@@ -17,6 +20,56 @@ script "Arc_Main" (int arcType)
         SetActorState(0, "Arc_EndArc", false);
         terminate;
     }
+    
+    // steps 5 to 9
+    SetActorState(0, "Arc_Look", false);
+    
+    // step 10
+    SetActivator(0, AAPTR_TRACER);
+    if (ClassifyActor(0) & ACTOR_WORLD)
+    {
+        SetActivator(arcerTID);
+        ACS_NamedExecuteWithResult("Arc_InheritTracer");
+        SetActorState(0, "Arc_NoTarget", false);
+        terminate;
+    }
+    
+    // step 11
+    SetActorState(arcerTID, "Arc_FoundTarget", false);
+    
+    // step 12
+    int tracerX      = GetActorX(0);
+    int tracerY      = GetActorY(0);
+    int tracerZ      = GetActorZ(0);
+    int tracerHeight = GetActorProperty(0, APROP_Height);
+    int tracerRadius = GetActorProperty(0, APROP_Height);
+    
+    SetActivator(arcerTID, AAPTR_TARGET);
+    int arcX     = tracerX;
+    int arcY     = tracerY;
+    int arcZ     = tracerZ + (tracerHeight / 2);
+    int arcAngle = GetActorAngle(0);
+    int arcPitch = GetActorPitch(0);
+    
+    str arcActor = STR_ArcData[arcType][ARC_S_NEXT];
+    int arcTID   = UniqueTID();
+    
+    SpawnForced(arcActor, arcX, arcY, arcZ, arcTID);
+    SetActorAngle(arcTID, arcAngle);
+    SetActorPitch(arcTID, arcPitch);
+    
+    // step 13
+    int targetTID_old = ActivatorTID();
+    int targetTID_new = UniqueTID();
+    Thing_ChangeTID(0, targetTID_new);
+    
+    SetActivator(arcTID);
+    SetPointer(AAPTR_TARGET, targetTID_new);
+    SetPointer(AAPTR_MASTER, arcerTID);
+    Thing_ChangeTID(targetTID_new, targetTID_old);
+    
+    // step 14
+    SetActorState(0, "Arc_Spawn", false);
 }
 
 
