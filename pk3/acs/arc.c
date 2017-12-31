7b285219c9d2e3c12be312f996aa45b3438ee181
0.0repeating <weranoutofgoodnames@gmail.com>
2017-12-31 12:42:48 -0600

    always good to write your postmortem ahead of time

--- pk3/acs/arc.c
+++ pk3/acs/arc.c
@@ -1,3 +1,28 @@
 #library "arc"
 #include "zcommon.acs"
+#include "dakka_commonFuncs.h"
 
+// Arc code, take three. This time, I try to minimize TID use as much as I can,
+//  because the previous arc code irreversibly trashed the TIDs of everything it
+//  looked at and I can't see any way to fix it without ripping out the logic
+//  entirely.
+//
+// Instead of using A_Look and keeping track of what's been seen through TIDs,
+//  I will use A_RadiusGive and run the target-weighing scripts on them, using
+//  SetPointer and user variables to keep track of the best target so far.
+// 
+// Instead of keeping track of previously hit targets in a TID array, I will
+//  iterate through the previous arcers and check their AAPTR_TRACER fields to
+//  see if we have a match.
+//
+// Hopefully this works out better. We can only hope.
+
+
+// ZDoom's no-team index (255) is different than Zandronum's no-team index (4).
+int NoTeamIndex;
+
+script "Arc_Open" open
+{   
+    SetDBEntry("dakka_arc", "client_iszand", 789237);
+    NoTeamIndex = cond(GetDBEntry("pickup", "client_iszand") == 789237, 4, 255);
+}
