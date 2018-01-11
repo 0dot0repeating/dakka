cf7d7f930b192e8a2e786c1a3acc065b10a41037
ijon <segfaultsamurai@gmail.com>
2018-01-10 22:04:17 -0600

    I found out KILL scripts exist in Zandronum 3, so I don't need to replace monsters anymore!

--- pk3/acs/dakka_onkill.h
+++ pk3/acs/dakka_onkill.h
@@ -1,5 +1,12 @@
-script "Dakka_OnKill" (int points)
+script "Dakka_OnKill" KILL
 {
+	if(!CheckFlag(0, "ISMONSTER"))
+	{
+		terminate;
+	}
+    
+    str monName = GetActorClass(0);
+    int points  = ACS_NamedExecuteWithResult("Score_Lookup", monName);
     ACS_NamedExecuteWithResult("Dakka_Score", points);
     
     int spawnHP = GetActorProperty(0, APROP_SpawnHealth);
