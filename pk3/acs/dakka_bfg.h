08512ac15e07e5c02d1c9c898b555360b7abd234
ijon <segfaultsamurai@gmail.com>
2018-01-23 19:00:46 -0600

    Replaced BFG alt-fire with a new version - needs new sounds and proper balancing, but the mechanics work

--- pk3/acs/dakka_bfg.h
+++ pk3/acs/dakka_bfg.h
@@ -238,29 +238,190 @@ function int Dakka_GetNewTarget(int ptrTID)
     int targetTID_old = PickActor(0, myAngle, myPitch, 0x7FFFFFFF, 0, MF_SHOOTABLE, ML_BLOCKING | ML_BLOCKEVERYTHING, PICKAF_RETURNTID);
     int targetTID_new = UniqueTID();
     
+    int t = Timer();
+    
     if (PickActor(0, myAngle, myPitch, 0x7FFFFFFF, targetTID_new, MF_SHOOTABLE, ML_BLOCKING | ML_BLOCKEVERYTHING, PICKAF_FORCETID))
     {
         SetActivator(ptrTID);
         SetPointer(AAPTR_TRACER, targetTID_new);
-        SetUserVariable(ptrTID, "user_timeout", 36);
+        SetUserVariable(0, "user_timeout", t + 72);
         
         SetActivator(myTID_new);
         Thing_ChangeTID(targetTID_new, targetTID_old);
     }
     else
     {
-        SetUserVariable(ptrTID, "user_timeout", max(0, GetUserVariable(ptrTID, "user_timeout") - 1));
+        SetActivator(ptrTID, AAPTR_TRACER);
+        int targetDead = (ClassifyActor(0) & ACTOR_WORLD) || isDead(0);
+        int timedOut   = GetUserVariable(ptrTID, "user_timeout") <= t;
+        SetActivator(myTID_new);
         
-        if (GetUserVariable(ptrTID, "user_timeout") == 0)
+        if (targetDead || timedOut)
         {
+            // I feel like this shouldn't work because this starts right on top of us
+            //   and yet it does work
+            
+            SpawnForced("BFGCrosshairTarget", GetActorX(0), GetActorY(0), GetActorZ(0) + GetActorViewHeight(0), targetTID_new);
+            SetActorVelocity(targetTID_new, 32767 * FixedMul(cos(myAngle), cos(myPitch)),
+                                            32767 * FixedMul(sin(myAngle), cos(myPitch)),
+                                            32767 * -sin(myPitch), false, false);
+            
             SetActivator(ptrTID);
-            SetPointer(AAPTR_TRACER, 0);
+            SetPointer(AAPTR_TRACER, targetTID_new);
+            Thing_ChangeTID(targetTID_new, 0);
+            
             SetActivator(myTID_new);
         }
     }
-    
+
     SetActorState(ptrTID, "KeepAlive");
     SetInventory("BFGPointerTID", ptrTID);
     Thing_ChangeTID(myTID_new, myTID_old);
     return ptrTID;
+}
+
+
+
+script "Dakka_SetupBFGStage2" (int facetracer, int speedadd, int speedmax, int evenIfBehind)
+{
+    int myTID_old = ActivatorTID();
+    int myTID_new = UniqueTID();
+    Thing_ChangeTID(0, myTID_new);
+    
+    SetActivator(0, AAPTR_TARGET);
+    if (ClassifyActor(0) & ACTOR_WORLD)
+    {
+        Thing_ChangeTID(myTID_new, myTID_old);
+        SetResultValue(false);
+        terminate;
+    }
+    
+    SetActivator(CheckInventory("BFGPointerTID"), AAPTR_TRACER);
+    if (ClassifyActor(0) & ACTOR_WORLD)
+    {
+        Thing_ChangeTID(myTID_new, myTID_old);
+        SetResultValue(false);
+        terminate;
+    }
+    
+    int targetTID_old = ActivatorTID();
+    int targetTID_new = UniqueTID();
+    Thing_ChangeTID(0, targetTID_new);
+    
+    SetActivator(myTID_new);
+    SetPointer(AAPTR_TRACER, targetTID_new);
+    
+    int dx = GetActorX(targetTID_new) - GetActorX(0);
+    int dy = GetActorY(targetTID_new) - GetActorY(0);
+    int dz = GetActorZ(targetTID_new) - GetActorZ(0) + (GetActorProperty(targetTID_new, APROP_Height) / 2);
+    
+    if (facetracer)
+    {
+        SetActorAngle(0,  VectorAngle(dx, dy));
+        SetActorPitch(0, -VectorAngle(VectorLength(dx, dy), dz));
+    }
+    
+    Thing_ChangeTID(myTID_new,     myTID_old);
+    Thing_ChangeTID(targetTID_new, targetTID_old);
+    SetResultValue(true);
+}
+
+
+
+script "Dakka_BFGHomeIn" (int speedadd, int speedmax, int evenIfBehind)
+{
+    speedadd = itof(speedadd);
+    speedmax = itof(speedmax);
+    
+    int myTID_old = ActivatorTID();
+    int myTID_new = UniqueTID();
+    Thing_ChangeTID(0, myTID_new);
+    
+    int x = GetActorX(0);
+    int y = GetActorY(0);
+    int z = GetActorZ(0);
+    
+    int angle = GetActorAngle(0);
+    int pitch = GetActorPitch(0);
+    
+    int aimX = FixedMul(cos(angle), cos(pitch));
+    int aimY = FixedMul(sin(angle), cos(pitch));
+    int aimZ = -sin(pitch);
+    
+    int tx,ty,tz;
+    
+    SetActivator(0, AAPTR_TRACER);
+    if (ClassifyActor(0) & ACTOR_WORLD)
+    {
+        if (GetUserVariable(myTID_new, "user_hadtarget"))
+        {
+            tx = GetUserVariable(myTID_new, "user_targetx");
+            ty = GetUserVariable(myTID_new, "user_targety");
+            tz = GetUserVariable(myTID_new, "user_targetz");
+        }
+        else // I guess just keep going then?
+        {
+            tx = x + FixedMul(speedmax, aimX);
+            ty = y + FixedMul(speedmax, aimY);
+            tz = z + FixedMul(speedmax, aimZ);
+        }
+    }
+    else
+    {
+        tx = GetActorX(0);
+        ty = GetActorY(0);
+        tz = GetActorZ(0) + (GetActorProperty(0, APROP_Height) / 2);
+        
+        SetUserVariable(myTID_new, "user_hadtarget", true);
+        SetUserVariable(myTID_new, "user_targetx", tx);
+        SetUserVariable(myTID_new, "user_targety", ty);
+        SetUserVariable(myTID_new, "user_targetz", tz);
+    }
+    
+    int dx = tx - x;
+    int dy = ty - y;
+    int dz = tz - z;
+    
+    SetActivator(myTID_new);
+    
+    if (!evenIfBehind)
+    {
+        int distInFront = dot3(dx,dy,dz, aimX,aimY,aimZ);
+        
+        if (distInFront < 0)
+        {
+            SetResultValue(false);
+            Thing_ChangeTID(myTID_new, myTID_old);
+            terminate;
+        }
+    }
+    
+    int dmag = VectorLength(VectorLength(dx, dy), dz);
+    int ndx = 0, ndy = 0, ndz = 0;
+    
+    if (dmag > 0)
+    {
+        ndx = FixedDiv(dx, dmag);
+        ndy = FixedDiv(dy, dmag);
+        ndz = FixedDiv(dz, dmag);
+    }
+    
+    int vx  = GetActorVelX(0) + FixedMul(ndx, speedadd);
+    int vy  = GetActorVelY(0) + FixedMul(ndy, speedadd);
+    int vz  = GetActorVelZ(0) + FixedMul(ndz, speedadd);
+    int vel = VectorLength(VectorLength(vx, vy), vz);
+    
+    if (speedmax > 0 && vel > speedmax)
+    {
+        int vdiff = FixedDiv(speedmax, vel);
+        vx = FixedMul(vx, vdiff);
+        vy = FixedMul(vy, vdiff);
+        vz = FixedMul(vz, vdiff);
+    }
+    
+    SetActorVelocity(0, vx,vy,vz, false,false);
+    SetActorAngle(0,  VectorAngle(vx,vy));
+    SetActorPitch(0, -VectorAngle(VectorLength(vx,vy), vz));
+    
+    SetResultValue(true);
 }
