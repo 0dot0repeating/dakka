f05b1b48d6c78d8566b7de7a85f90ea520b85d5a
ijon <segfaultsamurai@gmail.com>
2018-02-05 03:03:18 -0600

    Ammo consumption is now by ACS script, which keeps clients in better sync with server online

--- pk3/acs/dakka_online.h
+++ pk3/acs/dakka_online.h
@@ -1,5 +1,6 @@
 script "Dakka_AmmoSync" (int ammoindex)
 {
+    if (!IsServer) { terminate; }
     if (!(IsZandronum && ConsolePlayerNumber() == -1)) { terminate; }
     if (ammoindex < 0 || ammoindex >= AMMOCOUNT) { terminate; }
 
@@ -16,6 +17,24 @@ script "Dakka_AmmoSync" (int ammoindex)
         GiveAmmo(ammotype, 1);
     }
 }
+
+// This is purely to avoid ammo desyncs online.
+//
+// Zandronum clients predict when they're going to use ammo, and take it away
+// themselves. The server therefore never bothers to tell them they used ammo,
+// which usually works out... but with Dakka, it doesn't. At all. So we gotta
+// handle this manually.
+
+script "Dakka_UseAmmo" (int ammoindex, int count)
+{
+    if (!IsServer) { terminate; }
+    if (ammoindex < 0 || ammoindex >= AMMOCOUNT) { terminate; }
+    if (GetCVar("sv_infiniteammo") || CheckInventory("PowerInfiniteAmmo")) { terminate; }
+    
+    str ammotype = PKP_KnownAmmo[ammoindex];
+    TakeInventory(ammotype, count);
+}
+
 
 
 script "Dakka_PlayerNumber" (int ptr)
