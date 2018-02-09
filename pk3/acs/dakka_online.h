97f058842cfc7d55defc8f30c1f4524562690a4a
ijon <segfaultsamurai@gmail.com>
2018-02-09 14:48:28 -0600

    Added code to resolve the held weapon disagreements online as swiftly as possible

--- pk3/acs/dakka_online.h
+++ pk3/acs/dakka_online.h
@@ -1,23 +1,3 @@
-script "Dakka_AmmoSync" (int ammoindex)
-{
-    if (!IsServer) { terminate; }
-    if (!(IsZandronum && ConsolePlayerNumber() == -1)) { terminate; }
-    if (ammoindex < 0 || ammoindex >= AMMOCOUNT) { terminate; }
-
-    str ammotype = PKP_KnownAmmo[ammoindex];
-
-    if (CheckInventory(ammotype) == 0)
-    {
-        GiveAmmo(ammotype, 1);
-        TakeInventory(ammotype, 1);
-    }
-    else
-    {
-        TakeInventory(ammotype, 1);
-        GiveAmmo(ammotype, 1);
-    }
-}
-
 // This is purely to avoid ammo desyncs online.
 //
 // Zandronum clients predict when they're going to use ammo, and take it away
@@ -33,4 +13,44 @@ script "Dakka_UseAmmo" (int ammoindex, int count)
 
     str ammotype = PKP_KnownAmmo[ammoindex];
     TakeInventory(ammotype, count);
+}
+
+
+// Sometimes, the client thinks he switched weapons, and the server disagrees.
+//  Normally, this only gets resolved when the player tries to shoot, and finds
+//  that nothing is actually coming out of the barrel until a short bit later.
+//  This resolves the issue as soon as possible.
+
+script "Dakka_SyncHeldWeapon" enter clientside
+{
+    if (IsServer) { terminate; }
+    SetHudSize(640, 480, true);
+    
+    int lastWepIndex;
+    int curWepIndex;
+    int i, first = true;
+    
+    while (true)
+    {
+        str wep = GetWeapon();
+        curWepIndex = -1;
+        
+        for (i = 0; i < WEAPONCOUNT; i++)
+        {
+            if (!stricmp(PKP_KnownGuns[i][0], wep))
+            {
+                curWepIndex = i;
+                break;
+            }
+        }
+        
+        if (!first && (curWepIndex != lastWepIndex))
+        {
+            NamedRequestScriptPuke("Dakka_SwitchingTo", curWepIndex, 1);
+        }
+        
+        lastWepIndex = curWepIndex;
+        first = false;
+        Delay(1);
+    }
 }
