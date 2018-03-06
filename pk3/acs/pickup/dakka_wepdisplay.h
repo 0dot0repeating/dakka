ee899fbc7cb5fe4e0e4700369889aa72e1818237
ijon <segfaultsamurai@gmail.com>
2018-03-06 08:14:41 -0600

    Removed some useless relics from the pickup system

--- pk3/acs/pickup/dakka_wepdisplay.h
+++ pk3/acs/pickup/dakka_wepdisplay.h
@@ -6,24 +6,21 @@
 
 #define WDSTATE_NORMAL      0
 #define WDSTATE_SECONDWEP   1
-#define WDSTATE_BUNDLED     2
 
-int WepDisplay_States[WEPDISP_TYPES][3] =
+int WepDisplay_States[WEPDISP_TYPES][2] =
 {
-    {"Dakka", "SuperShotgun",   ""},
+    {"Dakka", "SuperShotgun"},
 };
 
-int WepDisplay_ReturnIndex[WEPDISP_TYPES][3] =
+int WepDisplay_ReturnIndex[WEPDISP_TYPES][2] =
 {
-    {It_Shotgun,        It_SuperShotgun,    -1},
+    {It_Shotgun,        It_SuperShotgun},
 };
 
-#define WDCHECK_USESECONDWEP    0
-#define WDCHECK_USEBUNDLE       1
 
-int WepDisplay_S2CCheck[WEPDISP_TYPES][2] =
+int WepDisplay_S2CCheck[WEPDISP_TYPES] =
 {
-    {S2C_D_SHOT2SSG,    -1},
+    S2C_D_SHOT2SSG
 };
 
 
@@ -45,8 +42,7 @@ script "Dakka_WepDisplay" (int weptype)
 
     if (classNum_client != Cl_Dakkaguy) { terminate; }
 
-    int secondWepCheck = WepDisplay_S2CCheck[weptype][WDCHECK_USESECONDWEP];
-    int bundleCheck    = WepDisplay_S2CCheck[weptype][WDCHECK_USEBUNDLE];
+    int secondWepCheck = WepDisplay_S2CCheck[weptype];
 
     int useSecond_client = false;
     int bundle_client    = false;
@@ -56,35 +52,20 @@ script "Dakka_WepDisplay" (int weptype)
         useSecond_client    = SToC_ClientData[cpln][secondWepCheck];
     }
 
-    if (bundleCheck > -1)
-    {
-        bundle_client       = SToC_ClientData[cpln][bundleCheck];
-    }
-
     // Gotta track this shit by inventory item so that every pickup changes
     //  properly - using an array had just one of the pickups change, and
     //  the rest of them to go "well the values match, time to do nothing :^)"
     int oldSecond = CheckInventory("DPickup_SecondWepState");
-    int oldBundle = CheckInventory("DPickup_BundleState");
+    if (oldSecond == useSecond_client) { terminate; }
 
-    if ((oldBundle == bundle_client) && (oldSecond == useSecond_client)) { terminate; }
-
-    SetInventory("DPickup_BundleState",     bundle_client);
     SetInventory("DPickup_SecondWepState",  useSecond_client);
 
-    if (bundle_client)
+    if (useSecond_client)
     {
-        Log(s:"switch to bundle (", s:WepDisplay_States[weptype][WDSTATE_BUNDLED], s:")");
-        SetActorState(0, WepDisplay_States[weptype][WDSTATE_BUNDLED]);
-    }
-    else if (useSecond_client)
-    {
-        Log(s:"switch to second (", s:WepDisplay_States[weptype][WDSTATE_SECONDWEP], s:")");
         SetActorState(0, WepDisplay_States[weptype][WDSTATE_SECONDWEP]);
     }
     else
     {
-        Log(s:"switch to normal (", s:WepDisplay_States[weptype][WDSTATE_NORMAL], s:")");
         SetActorState(0, WepDisplay_States[weptype][WDSTATE_NORMAL]);
     }
 }
@@ -93,37 +74,23 @@ script "Dakka_WepPickup" (int weptype)
 {
     int pln = PlayerNumber();
 
-    int classNum_server  = Pickup_ClassNumber(0);
-    int secondWepCheck = WepDisplay_S2CCheck[weptype][WDCHECK_USESECONDWEP];
-    int bundleCheck    = WepDisplay_S2CCheck[weptype][WDCHECK_USEBUNDLE];
+    int classNum_server = Pickup_ClassNumber(0);
+    int secondWepCheck  = WepDisplay_S2CCheck[weptype];
 
     int useSecond_server = false;
-    int bundle_server    = false;
 
     if (secondWepCheck > -1)
     {
         useSecond_server    = SToC_ServerData[pln][secondWepCheck];
     }
 
-    if (bundleCheck > -1)
-    {
-        bundle_server       = SToC_ServerData[pln][bundleCheck];
-    }
-
 
     int normPickup   = WepDisplay_ReturnIndex[weptype][WDSTATE_NORMAL];
     int secondPickup = WepDisplay_ReturnIndex[weptype][WDSTATE_SECONDWEP];
-    int bundlePickup = WepDisplay_ReturnIndex[weptype][WDSTATE_BUNDLED];
 
     if (classNum_server != Cl_Dakkaguy)
     {
         SetResultValue(PKP_PickupData[PDATA_ITEMNUM]);
-        terminate;
-    }
-
-    if (bundle_server)
-    {
-        SetResultValue(bundlePickup);
         terminate;
     }
 
@@ -135,6 +102,3 @@ script "Dakka_WepPickup" (int weptype)
 
     SetResultValue(normPickup);
 }
-
-
-script "printTimer" (void) { Log(s:"t: ", d:Timer()); }
